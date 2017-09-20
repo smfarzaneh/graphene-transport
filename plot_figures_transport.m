585c2a6025bb07c80a180ec1e4136b4b8c8ffe76
smfarzaneh <smf552@nyu.edu>
2017-09-20 16:06:27 -0400

    made a function to generate all the plots

---
+++ plot_figures_transport.m
@@ -0,0 +1,16 @@
+% MATLAB R2015a function
+% S. M. Farzaneh, farzaneh@nyu.edu
+% Created: September 19, 2017
+% Title: Plot all the figures related to electronic transport 
+% in bilayer graphene
+
+function [] = plot_figures_transport()
+
+	% Plot polarizability figures
+	plot_polarizability_bilayer();
+
+	% Plot momentum relaxation time figures
+	% it actually plots scattering rates
+	plot_relaxation_bilayer()
+
+end
