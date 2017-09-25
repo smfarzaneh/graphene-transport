38483db7179142e6191029828baa8a735d0f5ede
smfarzaneh <smf552@nyu.edu>
2017-09-25 18:13:41 -0400

    made calculations faster by vectorization

--- momentum_energy_bilayer.m
+++ momentum_energy_bilayer.m
@@ -23,37 +23,18 @@ func    = @(E, pm) 1/(hbar^2*vf^2)*(E.^2 + delta^2/4 + ...
 sub1min = gamma1*delta/(2*sqrt(gamma1^2 + delta^2));
 sub2min = sqrt(gamma1^2 + delta^2/4);
 num     = length(E); 
-k1      = 0*E - 1;
-alpha1 	= 0*E;
-k2      = 0*E - 1;
-alpha2 	= 0*E;
+alpha1 	= 0*E + 1;
+alpha2 	= 0*E + 1;
 
-% check if there is any answer
-for i = 1:num
-    if (abs(E(i)) >= sub1min)
-    	if (abs(E(i)) < sub2min)
-    		temp = func(E(i), -1);
-	        if (temp >= 0) 
-	        	k1(i) = sqrt(temp); 
-	        	alpha1(i) = 1;
-	        end
-	        temp = func(E(i), 1);
-	        if (temp >= 0) 
-	        	k2(i) = sqrt(temp); 
-	        	alpha2(i) = 1;
-	        end
-    	else
-	        temp = func(E(i), -1);
-	        if (temp >= 0) 
-	        	k1(i) = sqrt(temp); 
-	        	alpha1(i) = 2;
-	        end
-	        temp = func(E(i), 1);
-	        if (temp >= 0) 
-	        	k2(i) = sqrt(temp); 
-	        	alpha2(i) = 1;
-	        end
-        end
-    end
-end
+alpha1 	= alpha1.*heaviside(abs(E) - sub1min).*(1 - heaviside(abs(E) - delta/2)) + ...
+	2*heaviside(abs(E) - sub2min);
+alpha2 	= alpha2.*heaviside(abs(E) - sub1min);
+k1 		= sqrt(abs(func(E, -1))).*heaviside(abs(E) - sub1min) + ... 
+	(1 - heaviside(abs(E) - sub1min));
+k2 		= sqrt(abs(func(E, 1))).*heaviside(abs(E) - sub1min) + ...
+	(1 - heaviside(abs(E) - sub1min));
+% the last terms in k1 and k2 expressions are to avoid division by zero errors.
+% this extra term would not change the density of states at all because the alpha1 
+% and alpha2 vectors will mask k1 and k2 accordingly.
+
 end
