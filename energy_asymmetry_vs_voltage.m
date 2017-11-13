6c4193782716df64cd9579fdcd35dbc186d8e3cb
smfarzaneh <smf552@nyu.edu>
2017-11-13 16:06:23 -0500

    included impurity term

--- energy_asymmetry_vs_voltage.m
+++ energy_asymmetry_vs_voltage.m
@@ -4,18 +4,20 @@
 % Title: Calculate energy asymmetry of bilayer graphene for 
 % a given voltage and carrier imbalance
 
-function [del] = energy_asymmetry_vs_voltage(v, dn, tox, eox)
+function [del] = energy_asymmetry_vs_voltage(v, dn, tox, eox, nimp)
 	% del: energy asymmetry [J]
 	% v: top gate voltage [V]
 	% dn: carrier imbalance [1/m^2]
 	% tox: oxide thickness [m]
 	% eox: oxide relative permittivity [unitless]
+	% nimp: impurity density (positive for positive charged impurities) [1/m^2]
 
 	init_constant;         	% initialize physical constants and parameters
 	d = b; 					% distance between layers [m]
-	external_term = v*qe*d/(tox);
-	internal_term = dn*qe^2*d/(2*eps0*eox);	
+	ng = gate_carrier(v, tox, eox);
+	del0 = qe^2*(ng - nimp)*d/(eps0*eox);
+	del_correction = qe^2*dn*d/(2*eps0*eox);	
 
-	del = external_term - internal_term;
+	del = del0 - del_correction;
 
 end
