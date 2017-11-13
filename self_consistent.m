e96052fa105b2e8b084e391e8a93e4d3007ca78d
smfarzaneh <smf552@nyu.edu>
2017-11-13 18:59:13 -0500

    made it faster by inserting break statement in the loop

--- self_consistent.m
+++ self_consistent.m
@@ -5,7 +5,7 @@
 % for given gate voltage and temperature 
 % using self consistent Hartree approximation. 
 
-function [ef, del, n] = self_consistent(v, tox, eox, T, nimp)
+function [ef, del, ntot] = self_consistent(v, tox, eox, T, nimp)
 	% ef: Fermi level [J]
 	% del: energy asymmetry [J]
 	% v: top gate voltage [V]
@@ -25,7 +25,11 @@ function [ef, del, n] = self_consistent(v, tox, eox, T, nimp)
 	for i = 1:iteration
  		del = energy_asymmetry_vs_voltage(v, dn, tox, eox, nimp);
  		ef = fermi_bisection(ntot, del, T);
- 		[dn, n1, n2] = carrier_imbalance(ef, del, T);
+ 		[deltan, n1, n2] = carrier_imbalance(ef, del, T);
+ 		if(abs(deltan - dn)/deltan < 1e-6)
+ 			break
+ 		else
+ 			dn = deltan
  	end
 
 end
